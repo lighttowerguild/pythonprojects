bbafde3cde1952beffbb02f3fd5788bf74a1171a
cbw29512 <divclass01@gmail.com>
2021-02-12 00:25:43 -0500

    pythonbootcamp.py

---
+++ tiny_projects/pythonbootcamp.py
@@ -0,0 +1,54 @@
+# Data types
+# Int                 Whole numbers
+# Floating point      numbers with decimals
+# strings             "ordered sequence"
+# List                [10, "Sammy", "2000"] objects
+# Dictionaries        Key:value pair {"mykey": "value", "name": "Chris"}
+# Tuples              Immutable sequences (10, "hello", 200.3)
+# Sets                collection of unique objects {"a", "b"}
+# Boolean             Logical value True or False 
+
+# Numbers
+
+# Modulo Mod operator returns remainder after division
+# 7 % 4 returns 3
+#  50 % 5 returns 0
+#  2 ** 3 returns 8  2 x 2 x 2
+# Basic order of operations 
+# P stands for Parentheses
+# E stands for Exponents
+# M stands for Multiply
+# D stands for Division
+# A stands for Add
+# S stands for Subtraction
+
+# my_dog = 2
+# rules variable names: use _ instead of spaces
+# lower case name
+# avoid words with special meaning in python 
+# Python uses dynamic typing reassign variables to different data types
+# Strings are ordered sequences used for index and slicing 
+# Indexing notion uses [] notion after the string
+# Indexing allows you to grab single characters from the string
+# Characters:       h  e  l  l  o 
+# Index:            0  1  2  3  4
+# reverse index:    0 -4 -3 -2 -1 
+# slicing:      [start:stop:step] 
+# start numerical infex
+# stop index you go up to
+# step size of the jump
+# len checks length of string
+# indexing slicing with strings
+# reverse string mystring[::-1]
+# string properties and methods
+# immutability strings
+# error is attempt to concantenate a number with a string
+# .split() easy way to get a list from a string
+# String formatting for printing
+# inject variable into string; string interpolation
+#  .format() method and f-strings
+# print('The {2} {1} {0}'.format('fox', 'brown', 'quick')) return The quick brown fox
+# Float formating follows "{value:width.precision f}"
+#   result = 100/177 returned 0.1287001287001287
+#   print("The result was {r:1.3f}".format(r=result)) result 0.129
+# List [] ordered sequence supports index, slicing, nested 
